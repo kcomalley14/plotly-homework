2312c945f327cb08f8744e2e20a52ba71074189c
Kyle O'Malley <kyleomalley@Kyles-MBP-43.lan>
2020-12-30 18:49:31 -0500

    map values for plot

--- static/app.js
+++ static/app.js
@@ -11,10 +11,16 @@ function plot(data) {
         var label = sampledata.samples[0].otu_labels.slice(0,10);
         console.log(label);
 
-        var topTen = sampledata.samples[0].otu_ids.slice(0,10).reverse();
+        var topTen = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
         console.log(topTen);
 
-        var ;
-});
+        var topValues = (sampledata.otu_ids.slice(0,10)).reverse();
+        console.log(topValues);
+
+        var otuId = topValues.map(id => "OTU " + id);
+        console.log(`OTU IDs: ${otuId}`);
+
+        
+    });
 
 }
