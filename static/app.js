f345c75241f11ab0a7ba040d4c3f72649e9f4071
Kyle O'Malley <kyleomalley@Kyles-MBP-35.lan>
2020-12-29 21:50:58 -0500

    udpate js file

--- static/app.js
+++ static/app.js
@@ -1,4 +1,7 @@
 function plot(data)
 d3.json("../data/samples.json").then(sampledata => {
-    console.log(sampledata)
+    console.log(sampledata);
+
+    var ids = sampledata.samples[0].otu_ids;
+    console.log(ids);
 })
