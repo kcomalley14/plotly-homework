98f808ddb0c2bb423e2c64488b7f0cd29e20732e
Kyle O'Malley <kyleomalley@Kyles-MBP-35.lan>
2020-12-29 22:41:39 -0500

    start javascript

--- static/app.js
+++ static/app.js
@@ -1,7 +1,20 @@
-function plot(data)
-d3.json("../data/samples.json").then(sampledata => {
-    console.log(sampledata);
+function plot(data) {
+    d3.json("samples.json").then(sampledata => {
+        console.log(sampledata);
 
-    var ids = sampledata.samples[0].otu_ids;
-    console.log(ids);
-})
+        var ids = sampledata.samples[0].otu_ids;
+        console.log(ids);
+
+        var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues);
+
+        var label = sampledata.samples[0].otu_labels.slice(0,10);
+        console.log(label);
+
+        var topTen = sampledata.samples[0].otu_ids.slice(0,10).reverse();
+        console.log(topTen);
+
+        var ;
+});
+
+}
