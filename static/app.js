c55549b9cc60e4519126d9cedc2c6e5b139619aa
Kyle O'Malley <kyleomalley@Kyles-MBP-43.lan>
2020-12-30 19:36:33 -0500

    found typo in plot function

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 function plot(data) {
-    d3.json("../data/samples.json").then(sampledata => {
+    d3.json("../data/samples.json").then((sampledata) => {
         console.log(sampledata);
 
         var ids = sampledata.samples[0].otu_ids;
@@ -14,20 +14,20 @@ function plot(data) {
         var topTen = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
         console.log(topTen);
 
-        var topValues = (sampledata.otu_ids.slice(0,10)).reverse();
-        console.log(topValues);
+        // var topValues = (sampledata.otu_ids.slice(0,10)).reverse();
+        // console.log(topValues);
 
-        var otuId = topValues.map(id => "OTU " + id);
+        var otuId = topTen.map(id => "OTU " + id);
         console.log(`OTU IDs: ${otuId}`);
 
         console.log(`Sample Values: ${sampleValues}`)
-        console.log(`Id Values: ${idValues}`)
+        console.log(`Id Values: ${topTen}`)
 
         // trace variable
         var trace = {
             x: sampleValues,
             y: otuId,
-            text: labels,
+            text: label,
             type: 'bar',
             orientation: 'h'
         };
@@ -39,6 +39,36 @@ function plot(data) {
         };
 
         Plotly.newPlot("bar", data, layout);
-    });
+
+
+    }).catch(err => {
+        console.log(err);
+      });
 
 }
+plot();
+
+
+// create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("../data/samples.json").then((data)=> {
+        console.log(data)
+
+        // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+        // call the functions to display the data and the plots to the page
+        plot(data.names[0]);
+        getInfo(data.names[0]);
+    }).catch(err => {
+        console.log(err);
+      });
+}
+
+init();
